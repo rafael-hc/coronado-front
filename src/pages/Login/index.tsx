07ac070c15f98b44a11c85c21802163c46d0fb08
rafabrazil <rafael.h@outlook.com>
2022-09-13 08:57:57 -0300

    Tela de login em andamento

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import {
   FormSingIn,
   LoginContainer,
@@ -8,8 +7,6 @@ import {
 } from './styles'
 
 export const Login = () => {
-  const [number, setNumber] = useState('')
-  let numero = ''
   return (
     <LoginContainer>
       <h1>Acessar ou criar conta</h1>
@@ -22,7 +19,6 @@ export const Login = () => {
             name="login"
             id="login"
             placeholder="Digite seu e-mail ou CPF/CNPJ"
-            onChange={(e) => setNumber(e.target.value)}
           />
           <label htmlFor="">Senha</label>
           <input
@@ -30,10 +26,6 @@ export const Login = () => {
             name="password"
             id="password"
             placeholder="Digite sua senha"
-            onChange={(e) => {
-              numero = e.target.value
-              console.log(numero)
-            }}
           />
           <button type="submit">Entrar</button>
         </FormSingIn>
@@ -42,10 +34,6 @@ export const Login = () => {
         <h2>Crie sua conta é rápido, fácil e gratuito!</h2>
         <SingUpButton to="/register">Criar uma conta</SingUpButton>
       </SingUp>
-      <div>
-        <p>{`Variável de estado: ${number}`}</p>
-        <p>{`Variável normal: ${numero}`}</p>
-      </div>
     </LoginContainer>
   )
 }
