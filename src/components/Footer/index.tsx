2a7fb53779e24e1318e5b17cf25d53024897c22d
Mariana Hammel Brandão <Mariana@Mac-mini-de-Isabella.local>
2022-09-13 10:33:12 -0300

    footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,56 @@
-import { FooterContainer } from './styles'
+import {
+  FacebookLogo,
+  InstagramLogo,
+  EnvelopeSimple,
+  WhatsappLogo,
+  Phone,
+} from 'phosphor-react'
+import {
+  Contato,
+  FooterContainer,
+  RedeSocial,
+  SobreNos,
+  TrabalheConosco,
+} from './styles'
 
 export const Footer = () => {
   return (
     <FooterContainer>
-      <div>Sobre a Coronado</div>
-      <div>Dúvidas</div>
-      <div>Serviços</div>
-      <div>Siga a Coronado</div>
+      <SobreNos>
+        <a href="#">Sobre a Coronado</a>
+      </SobreNos>
+
+      <Contato>
+        <div>Contato</div>
+        <div className="iconesRedeSocial">
+          <a href="#">
+            <EnvelopeSimple size={24} />
+          </a>
+          <a href="#">
+            <WhatsappLogo size={24} />
+          </a>
+          <a href="#">
+            <Phone size={24} />
+          </a>
+        </div>
+      </Contato>
+
+      <TrabalheConosco>
+        <a href="#">Trabalhe Conosco</a>
+      </TrabalheConosco>
+
+      <RedeSocial>
+        <div>Siga a Coronado</div>
+        <div className="iconesRedeSocial">
+          <a href="#">
+            <FacebookLogo size={24} />
+          </a>
+
+          <a href="#">
+            <InstagramLogo size={24} />
+          </a>
+        </div>
+      </RedeSocial>
     </FooterContainer>
   )
 }
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,19 +1,85 @@
 import styled from 'styled-components'
 
 export const FooterContainer = styled.footer`
-  display: flex;
-  width: 100%;
-  height: 15rem;
-  padding: 2rem;
-  background-color: ${(props) => props.theme.footer};
+  background-color: ${(props) => props.theme['gray-400']};
 
   box-sizing: border-box;
 
+  max-width: 100%;
+  height: 12rem;
+  padding: 1rem;
+
   display: grid;
   grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr 1fr;
-  align-items: center;
+  grid-template-rows: 0.5fr 0.5fr;
+  gap: 2rem;
+`
+
+export const SobreNos = styled.div`
+  grid-column-start: 1;
+  grid-row-start: 1;
+
+  display: flex;
   justify-content: center;
+  align-items: center;
   gap: 1rem;
-  padding: 2rem;
+
+  a {
+    display: flex;
+  }
+`
+
+export const Contato = styled.div`
+  grid-column-start: 1;
+  grid-row-start: 2;
+
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+
+  a {
+    display: flex;
+  }
+
+  .iconesRedeSocial {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`
+
+export const TrabalheConosco = styled.div`
+  grid-column-start: 2;
+  grid-row-start: 1;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  gap: 1rem;
+
+  a {
+    display: flex;
+  }
+`
+
+export const RedeSocial = styled.div`
+  grid-column-start: 2;
+  grid-row-start: 2;
+
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+
+  a {
+    display: flex;
+  }
+
+  .iconesRedeSocial {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 `
