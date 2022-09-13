1a0d8a6f7c02901359ba832ff4e51643b78f36c9
rafabrazil <rafael.h@outlook.com>
2022-09-13 08:42:17 -0300

    Tela de login em andamento

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,23 +1,51 @@
-import { Link } from 'react-router-dom'
-import { LoginContainer, SingIn, SingUp } from './styles'
+import { useState } from 'react'
+import {
+  FormSingIn,
+  LoginContainer,
+  SingIn,
+  SingUp,
+  SingUpButton,
+} from './styles'
 
 export const Login = () => {
+  const [number, setNumber] = useState('')
+  let numero = ''
   return (
     <LoginContainer>
+      <h1>Acessar ou criar conta</h1>
       <SingIn>
         <h2>Acesse sua conta</h2>
-        <form action="">
+        <FormSingIn action="">
           <label htmlFor="">E-mail / CPF ou CNPJ</label>
-          <input type="text" name="login" id="login" />
+          <input
+            type="text"
+            name="login"
+            id="login"
+            placeholder="Digite seu e-mail ou CPF/CNPJ"
+            onChange={(e) => setNumber(e.target.value)}
+          />
           <label htmlFor="">Senha</label>
-          <input type="text" name="password" id="password" />
+          <input
+            type="text"
+            name="password"
+            id="password"
+            placeholder="Digite sua senha"
+            onChange={(e) => {
+              numero = e.target.value
+              console.log(numero)
+            }}
+          />
           <button type="submit">Entrar</button>
-        </form>
+        </FormSingIn>
       </SingIn>
       <SingUp>
         <h2>Crie sua conta é rápido, fácil e gratuito!</h2>
-        <Link to="/register">Criar uma conta</Link>
+        <SingUpButton to="/register">Criar uma conta</SingUpButton>
       </SingUp>
+      <div>
+        <p>{`Variável de estado: ${number}`}</p>
+        <p>{`Variável normal: ${numero}`}</p>
+      </div>
     </LoginContainer>
   )
 }
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 export const LoginContainer = styled.main`
@@ -6,12 +7,66 @@ export const LoginContainer = styled.main`
 
   display: grid;
   grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr 1fr;
+  grid-template-rows: 3rem 1fr;
+  gap: 2rem;
 `
 
 const LoginContent = styled.section`
   display: flex;
   flex-direction: column;
 `
-export const SingIn = styled(LoginContent)``
-export const SingUp = styled(LoginContent)``
+export const SingIn = styled(LoginContent)`
+  grid-column-start: 1;
+  grid-row-start: 2;
+  padding: 1rem;
+
+  border-radius: 8px;
+
+  background-color: ${(props) => props.theme['gray-400']};
+`
+
+export const SingUp = styled(LoginContent)`
+  grid-column-start: 2;
+  grid-row-start: 2;
+
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+`
+
+export const FormSingIn = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  input {
+    padding: 0.5rem;
+    border: none;
+
+    border-radius: 8px;
+  }
+
+  button {
+    height: 2.5rem;
+    border-radius: 8px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background-color: ${(props) => props.theme.secondary};
+    color: ${(props) => props.theme.white};
+  }
+`
+
+export const SingUpButton = styled(Link)`
+  height: 2.5rem;
+  border-radius: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: ${(props) => props.theme.secondary};
+  color: ${(props) => props.theme.white};
+`
